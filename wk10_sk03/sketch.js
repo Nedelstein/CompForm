27e45c0a2b22aa643cf83d3b8c5d7dc545175424
Noah Edelstein <Noedelstein@gmail.com>
2019-04-10 21:55:45 -0400

    spell checked txt file

--- wk10_sk03/sketch.js
+++ wk10_sk03/sketch.js
@@ -1,5 +1,3 @@
-// let txt =
-// "The evidence for a flat earth is derived from many different facets of science and philosophy. The simplest is by relying on ones own senses to discern the true nature of the world around us. The world looks flat, the bottoms of clouds are flat, the movement of the Sun; these are all examples of your senses telling you that we do not live on a spherical heliocentric world. This is using what's called an empirical approach, or an approach that relies on information from your senses. Alternatively, when using Descartes' method of Cartesian doubt to skeptically view the world around us, one quickly finds that the notion of a spherical world is the theory which has the burden of proof and not flat earth theory. Perhaps the best example of flat earth proof is the Bedford Level Experiment. In short, this was an experiment performed many times on a six-mile stretch of water that proved the surface of the water to be flat. It did not conform to the curvature of the earth that round earth proponents teach. ";
 let order = 4;
 let ngrams = {};
 let button;
